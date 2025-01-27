eb2fc9aa3fd3736962e75f43209d753bc891d755
Miko Elbrecht <tcgytsc@gmail.com>
2025-01-26 20:40:34 -0600

    Reorganize imports in main

--- src/main.adb
+++ src/main.adb
@@ -1,10 +1,11 @@
-with Ada.Text_IO; use Ada.Text_IO;
 with Interfaces;
-with System.Low_Level; use System.Low_Level;
-with System.CGA_TextMode; use System.CGA_TextMode;
 with System.PS2;
 with System.Relative_Delays;
 with System.RTC;
+
+with Ada.Text_IO; use Ada.Text_IO;
+with System.Low_Level; use System.Low_Level;
+with System.CGA_TextMode; use System.CGA_TextMode;
 
 procedure Main is
 begin
